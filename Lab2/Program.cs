d026efbb7e6d3d5a19e458d299259b60708dd62e
Kenan Casey <kenancasey@gmail.com>
2021-10-28 10:36:47 -0500

    initial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //IsBalanced("{ int a = new int[ ] ( ( ) ) }");
+
+            Evaluate("5 3 11 + -");
+
+        }
+
+
+
+        public static bool IsBalanced(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach( char c in s)
+            {
+                // If opening symbol, then push onto stack
+                if ( c == '{' || c=='<' || c=='[' || c=='(' )
+                {
+                    stack.Push(c);
+                }
+
+                // If closing symbol, then see if it matches the top
+                else if (c == '}' || c == '>' || c == ']' || c == ')')
+                {
+                    if( Matches(stack.Peek(), c) )
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                // If any other character, then continue/ignore it.
+                else
+                {
+                    //continue;
+                }
+            }
+
+            // If stack is empty, return true
+            // else return false
+
+            if( stack.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool Matches(char open, char close)
+        {
+            // do the matching
+
+            return true;
+        }
+
+        // Evaluate("5 3 11 + -")	// returns -9
+        // 2.4 3.8 / 2.321 +
+        
+        public static double? Evaluate(string s)
+        {
+            // parse into tokens (strings)
+
+            string[] tokens = s.Split();
+
+            Stack<double> stack = new Stack<double>();
+
+            // foreach token
+                // If token is an integer
+                // Push on stack
+
+                // If token is an operator
+                    // Pop twice and save both values
+                    // (if you can't pop twice, then return null)
+                    // Perform operation on 2 values (in the correct order)
+                    // Push the result on to stack
+
+
+            if( stack.Count != 1)
+            {
+                return null;
+            }
+
+            return stack.Pop();
+        }
+
+
+
+    }
+}
---
+++ UnitTests/Lab2Tests.cs
@@ -0,0 +1,195 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab2;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Lab2 Tests
+        #region
+        [TestMethod]
+        public void Parentheses1()
+        {
+            Assert.IsTrue(Lab2.Program.IsBalanced("{()}{}"));
+        }
+
+        [TestMethod]
+        public void Parentheses2()
+        {
+            Assert.IsTrue(Lab2.Program.IsBalanced("{ int a = new int[ ] ( ( ) ) }"));
+        }
+
+        [TestMethod]
+        public void Parentheses3()
+        {
+            
+            Assert.IsFalse(Lab2.Program.IsBalanced("{ [ ] ) ) ( ( "));
+        }
+
+        //this one has the right number of corresponding braces but in the wrong order
+        [TestMethod]
+        public void Parentheses4()
+        {
+            Assert.IsFalse(Lab2.Program.IsBalanced("{[}]"));
+        }
+
+        [TestMethod]
+        public void Parentheses5()
+        {
+            Assert.IsFalse(Lab2.Program.IsBalanced("{"));
+        }
+
+        [TestMethod]
+        public void Parentheses6()
+        {
+            
+            Assert.IsFalse(Lab2.Program.IsBalanced("("));
+        }
+
+        [TestMethod]
+        public void Parentheses7()
+        {
+            
+            Assert.IsFalse(Lab2.Program.IsBalanced("["));
+        }
+
+        [TestMethod]
+        public void Parentheses8()
+        {
+            
+            Assert.IsFalse(Lab2.Program.IsBalanced("}"));
+        }
+
+        [TestMethod]
+        public void Parentheses9()
+        {
+            
+            Assert.IsFalse(Lab2.Program.IsBalanced(")"));
+        }
+
+        [TestMethod]
+        public void Parentheses10()
+        {
+            
+            Assert.IsFalse(Lab2.Program.IsBalanced("]"));
+        }
+
+        [TestMethod]
+        public void Parentheses11()
+        {
+            string testString = "namespace UnitTestProject1 {[TestClass] public class UnitTest1 {[TestMethod] public void OneInt() { DoublyLinkedList<int> doublyLinkedList = new DoublyLinkedList<int>(); doublyLinkedList.Append(0); Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TwoInt() { DoublyLinkedList<int> doublyLinkedList = new DoublyLinkedList<int>(); doublyLinkedList.Append(0); doublyLinkedList.Append(0); Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void ThreeInt() { DoublyLinkedList<int> doublyLinkedList = new DoublyLinkedList<int>(); doublyLinkedList.Append(0); doublyLinkedList.Append(1); doublyLinkedList.Append(0); Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void ManyInt() { DoublyLinkedList<int> doublyLinkedList = new DoublyLinkedList<int>(); for (int i = -15; i < 16; i++) { doublyLinkedList.Append(Math.Abs(i)); } Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TwoIntF() { DoublyLinkedList<int> doublyLinkedList = new DoublyLinkedList<int>(); doublyLinkedList.Append(0); doublyLinkedList.Append(1); Assert.IsFalse(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void ThreeIntF() { DoublyLinkedList<int> doublyLinkedList = new DoublyLinkedList<int>(); doublyLinkedList.Append(3); doublyLinkedList.Append(1); doublyLinkedList.Append(0); Assert.IsFalse(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void ManyIntF() { DoublyLinkedList<int> doublyLinkedList = new DoublyLinkedList<int>(); for (int i = 0; i < 16; i++) { doublyLinkedList.Append(Math.Abs(i)); } Assert.IsFalse(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestString1() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); doublyLinkedList.Append(alex); Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestString2() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); doublyLinkedList.Append(alex); doublyLinkedList.Append(alex); Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestString3() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); doublyLinkedList.Append(alex); doublyLinkedList.Append(inner); doublyLinkedList.Append(alex); Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestString4() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); for (int i = 2; i < 51; i++) { if (i % 2 == 0) { doublyLinkedList.Append(one); } else { doublyLinkedList.Append(two); } } Assert.IsTrue(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestStringUseEqualsF1() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); doublyLinkedList.Append(Alex); doublyLinkedList.Append(inner); doublyLinkedList.Append(alex); Assert.IsFalse(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestStringF2() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); doublyLinkedList.Append(Alex); doublyLinkedList.Append(inner); doublyLinkedList.Append(tanner); Assert.IsFalse(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestStringF3() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); doublyLinkedList.Append(inner); doublyLinkedList.Append(alex); Assert.IsFalse(Program.IsPalindrome(doublyLinkedList)); }[TestMethod] public void TestStringF4() { DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>(); for (int i = 3; i < 51; i++) { if (i % 3 == 0) { doublyLinkedList.Append(one); } else { doublyLinkedList.Append(two); } } doublyLinkedList.Append(three); Assert.IsFalse(Program.IsPalindrome(doublyLinkedList)); } } }";
+            Assert.IsTrue(Lab2.Program.IsBalanced(testString));
+        }
+
+        [TestMethod]
+        public void Parentheses12()
+        {
+            string testString = "List<int> list = new List<int>();";
+            Assert.IsTrue(Lab2.Program.IsBalanced(testString));
+        }
+
+        [TestMethod]
+        public void Parentheses13()
+        {
+            string testString = "List<int> list = new List<int();";
+            Assert.IsTrue(Lab2.Program.IsBalanced(testString));
+        }
+
+        [TestMethod]
+        public void Parentheses14()
+        {
+            string testString = "List<int> list = new List int>();";
+            Assert.IsTrue(Lab2.Program.IsBalanced(testString));
+        }
+
+        [TestMethod]
+        public void Parentheses1withNoBrace()
+        {
+            
+            Assert.IsTrue(Lab2.Program.IsBalanced("1"));
+        }
+
+        [TestMethod]
+        public void ParenthesesWithNoBraces()
+        {
+            
+            Assert.IsTrue(Lab2.Program.IsBalanced("When in the course of Human Events..."));
+        }
+        #endregion
+
+        //Postfix Tests
+        #region
+        [TestMethod]
+        public void Postfix1()
+        {
+            
+            Assert.AreEqual(4, Lab2.Program.Evaluate("2 2 +"));
+        }
+
+        [TestMethod]
+        public void Postfix2()
+        {
+            
+            Assert.AreEqual(8, Lab2.Program.Evaluate("5 3 +"));
+        }
+
+        [TestMethod]
+        public void Postfix3()
+        {
+            
+            Assert.AreEqual(2, Lab2.Program.Evaluate("7 5 -"));
+        }
+
+        [TestMethod]
+        public void Postfix4()
+        {
+            
+            Assert.AreEqual(1, Lab2.Program.Evaluate("5 3 1 + -"));
+        }
+
+        [TestMethod]
+        public void Postfix5()
+        {
+            
+            Assert.AreEqual(5, Lab2.Program.Evaluate("15 7 1 1 + - / 3 * 2 1 1 + + -"));
+        }
+
+        [TestMethod]
+        public void Postfix6()
+        {
+
+            Assert.AreEqual(45, Lab2.Program.Evaluate("15 7 1 2 + - / 3 * 2 1 1 + + *"));
+        }
+
+        [TestMethod]
+        public void Postfix7()
+        {
+
+            Assert.AreEqual(-3.4, Lab2.Program.Evaluate("16 15 - 7 1 1 + - / 3 * 2 1 1 + + -"));
+        }
+
+        [TestMethod]
+        public void Postfix8()
+        {
+
+            Assert.AreEqual(1.0/6.0, Lab2.Program.Evaluate("51 32 + 82 - 6 /"));
+        }
+
+        [TestMethod]
+        public void Postfix9()
+        {
+
+            Assert.AreEqual(2030, Lab2.Program.Evaluate("54 62 + 2 / 35 *"));
+        }
+
+        [TestMethod]
+        public void Postfix10()
+        {
+
+            Assert.AreEqual(23, Lab2.Program.Evaluate("10 2 8 * + 3 -"));
+        }
+        #endregion
+
+    }
+}
